980d9a39e18a2d958b5a2221d3c42d5365f4cd74
skl351 <skl1012401678@2980.com>
2017-12-25 13:20:56 +0800

    2017-12-25  code：1.0   1

---
+++ app/src/main/java/jt/ttaa/MainActivity.kt
@@ -0,0 +1,12 @@
+package jt.ttaa
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
